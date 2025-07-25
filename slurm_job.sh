233d9db92abe2269a6d0d390e72820bcad1ec969
Jiankai Wang <jw1524@ic.ac.uk>
2025-07-25 17:30:12 +0100

    update exp results for dropout dcrl

--- slurm_job.sh
+++ slurm_job.sh
@@ -22,9 +22,9 @@ rm -rf "$exp_path"
 mkdir -p "$exp_path"
 
 # damage rotation
-damaged_joint_idx=("0 1" "0 1" "4 5 6 7" "0 1 6 7")
-damaged_joint_action=("0 0.9" "-0.8 0.9" "0 0.9 0 0.9" "0 0.9 0 0.9")
-damage_desc=("FL_stiff" "FL_stiff_2" "BL_BR_stiff" "FL_BR_stiff")
+damaged_joint_idx=("0 1" "4 5" "4 5 6 7" "0 1 6 7")
+damaged_joint_action=("0 0" "0 0" "0 0 0 0" "0 0 0 0")
+damage_desc=("FL_loose" "BL_loose" "BL_BR_loose" "FL_BR_loose")
 
 for i in "${!damaged_joint_idx[@]}"; do
     echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
@@ -42,10 +42,10 @@ for i in "${!damaged_joint_idx[@]}"; do
 done
 
 
-exp_path="${output_path}/sensory_damage"
-rm -rf "$exp_path"
-mkdir -p "$exp_path"
-python main.py --config config.json --output_path $output_path --exp_path $exp_path --damage_type sensory
+# exp_path="${output_path}/sensory_damage"
+# rm -rf "$exp_path"
+# mkdir -p "$exp_path"
+# python main.py --config config.json --output_path $output_path --exp_path $exp_path --damage_type sensory
 
 # interactive job
 # srun --pty --gres=gpu:1 bash
