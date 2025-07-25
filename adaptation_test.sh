19be932d6304beb9319ab67390e7f6449663d463
Jiankai Wang <jw1524@ic.ac.uk>
2025-07-25 16:34:00 +0100

    update exp results

--- adaptation_test.sh
+++ adaptation_test.sh
@@ -1,5 +1,31 @@
 
 # output_path="outputs/slurm/dcrl_20250710_133450"
-# output_path="outputs/hpc/dcrl_20250723_160932"
-output_path="outputs/slurm/dcrl_20250723_175333"
-python main.py --config config.json --output_path $output_path  --exp_path $output_path --damage_type physical
+output_path="outputs/hpc/dcrl_20250723_160932"
+# output_path="outputs/hpc/mapelites_20250724_102129"
+# output_path="outputs/slurm/dcrl_20250723_175333"
+
+
+exp_path="${output_path}/physical_damage"
+mkdir -p "$exp_path"
+
+# damage rotation
+damaged_joint_idx=("4 5")
+damaged_joint_action=("0 0")
+damage_desc=("BL_stiff")
+
+for i in "${!damaged_joint_idx[@]}"; do
+    echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+    echo "Experiment ${damage_desc[$i]}:"
+
+    idx="${damaged_joint_idx[$i]}"
+    action="${damaged_joint_action[$i]}"
+    echo "Damage_joint_idx=$idx"
+    echo "Damaged_joint_action=$action"
+
+    damage_path="${exp_path}/${damage_desc[$i]}"
+    mkdir -p "$damage_path"
+    python main.py --config config.json --output_path $output_path --exp_path $damage_path --damage_joint_idx $idx --damage_joint_action $action --damage_type physical
+    
+done
+
+echo "%%%%%%%%%%%%%%%Running Complete%%%%%%%%%%%%%%%"
--- hpc_job.sh
+++ hpc_job.sh
@@ -10,7 +10,8 @@ source qdax050/bin/activate
 # python main.py --config config.json --algo_type dcrl --output_path outputs/hpc --mode training
 
 # # adaptation inspection
-output_path="outputs/hpc/mapelites_20250724_102129"
+# output_path="outputs/hpc/mapelites_20250724_102129"
+output_path="outputs/hpc/dcrl_20250723_160932"
 # python main.py --config config.json --output_path $output_path  --exp_path $output_path --damage_type physical
 
 exp_path="${output_path}/physical_damage"
@@ -18,9 +19,9 @@ rm -rf "$exp_path"
 mkdir -p "$exp_path"
 
 # damage rotation
-damaged_joint_idx=("0 1" "0 1" "4 5 6 7" "0 1 6 7")
-damaged_joint_action=("0 0.9" "-0.8 0.9" "0 0.9 0 0.9" "0 0.9 0 0.9")
-damage_desc=("FL_stiff" "FL_stiff_2" "BL_BR_stiff" "FL_BR_stiff")
+damaged_joint_idx=("0 1" "4 5" "4 5 6 7" "0 1 6 7")
+damaged_joint_action=("0 0" "0 0" "0 0 0 0" "0 0 0 0")
+damage_desc=("FL_loose" "BL_loose" "BL_BR_loose" "FL_BR_loose")
 
 for i in "${!damaged_joint_idx[@]}"; do
     echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
@@ -38,12 +39,9 @@ for i in "${!damaged_joint_idx[@]}"; do
 done
 
 
-exp_path="${output_path}/sensory_damage"
-rm -rf "$exp_path"
-mkdir -p "$exp_path"
-python main.py --config config.json --output_path $output_path --exp_path $exp_path --damage_type sensory
-
-# interactive job
-# srun --pty --gres=gpu:1 bash
+# exp_path="${output_path}/sensory_damage"
+# rm -rf "$exp_path"
+# mkdir -p "$exp_path"
+# python main.py --config config.json --output_path $output_path --exp_path $exp_path --damage_type sensory
 
 echo "%%%%%%%%%%%%%%%Running Complete%%%%%%%%%%%%%%%"
